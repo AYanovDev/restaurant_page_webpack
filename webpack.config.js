9491c29f413eab9f0f18e1d791fc11d45aba5d2f
AYanovDev <alex.ianovskii@mail.ru>
2025-01-10 21:22:11 +0500

    initial webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/template.html",
+    }),
+  ],
+};
